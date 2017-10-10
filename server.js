5c938d431e198721ae39e7f5c19360a7f8431836
chenghua <chehua263859@163.com>
2017-10-10 09:18:53 +0800

    fix pm2 hostname and port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
 const egg = require('egg')
 const os = require('os')
-const cpuLength = 2 // os.cpus().length
+const cpuLength = 1 // os.cpus().length
 
 const workers = Number(process.argv[2] || cpuLength)
 
 egg.startCluster({
     workers,
-    baseDir: __dirname
+    baseDir: __dirname,
+    hostname: '0.0.0.0',
+    port: 8000
 })
