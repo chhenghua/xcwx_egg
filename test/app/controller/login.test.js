282793bfd3036a8dc1f95a98b8b3d4c5cbfd1ccd
chenghua <chehua263859@163.com>
2017-10-13 17:05:46 +0800

    del test/app/*

--- test/app/controller/login.test.js
+++
@@ -1,11 +0,0 @@
-'use strict'
-const {app} = require('egg-mock/bootstrap')
-describe('test/app/controller/login.test.js', () => {
-    it('should POST /api/login', () => {
-        return app.httpRequest()
-            .post('/api/login')
-            .send({"username":"sss"})
-            .expect(200)
-    })
-})
-
